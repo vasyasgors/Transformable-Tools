c711cb28775a9cf0e283e3a55834899ca560f26b
Vasiliy <91281754+vasyasgors@users.noreply.github.com>
2026-02-28 01:37:10 +0300

    init

---
+++ Editor/Window/SceneTransformableToolsCollector.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace TransformableTools
+{
+    public class SceneTransformableToolsCollector : ScriptableObject
+    {
+        [SerializeField] private RandomRotationTool randomRotationTool;
+        [SerializeField] private RandomScaleTool randomScaleTool;
+        [SerializeField] private ResetRotationAndScaleTool resetRotationAndScaleTool;
+        [SerializeField] private SetOnFloorTool setOnFloorTool;
+
+        public void DrawAll()
+        {
+            randomRotationTool.Draw();
+            randomScaleTool.Draw();
+            resetRotationAndScaleTool.Draw();
+            setOnFloorTool.Draw();
+        }
+    }
+}
